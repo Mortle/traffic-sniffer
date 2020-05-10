838ee123bb4ff8391273ff679010511abe91e848
Mortle <alexander.vinogradov.an@mail.ru>
2020-05-10 08:04:23 +0300

    Initial sniffer implementation

---
+++ main.c
@@ -0,0 +1,54 @@
+#include "sniff.h"
+
+int main(int argc, char *argv[]) {
+  char *dev;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t* descr;
+  // const u_char *packet;
+  // struct pcap_pkthdr hdr;
+  // struct ether_header *eptr; /* net/ethernet.h        */
+  struct bpf_program fp;        /* hold compiled program */
+  bpf_u_int32 maskp;            /* subnet mask           */
+  bpf_u_int32 netp;             /* ip                    */
+
+  /* Requires filter argument */
+  if(argc != 2) {
+    fprintf(stdout, "Usage: %s \"expression\"\n", argv[0]);
+    return 0;
+  }
+
+  /* Get device */
+  dev = pcap_lookupdev(errbuf);
+
+  if(dev == NULL) {
+    fprintf(stderr, "%s\n", errbuf);
+    exit(1);
+  }
+
+  /* Get the network address and mask */
+  pcap_lookupnet(dev, &netp, &maskp, errbuf);
+
+  /* Open device for reading in promiscuous mode */
+  descr = pcap_open_live(dev, BUFSIZ, 1,-1, errbuf);
+  if(descr == NULL) {
+    printf("pcap_open_live(): %s\n", errbuf);
+    exit(1);
+  }
+
+  /* Compile the filter expression */
+  if(pcap_compile(descr, &fp, argv[1], 0, netp) == -1) {
+    fprintf(stderr, "Error calling pcap_compile\n");
+    exit(1);
+  }
+
+  /* Set the filter */
+  if(pcap_setfilter(descr, &fp) == -1) {
+    fprintf(stderr, "Error setting filter\n");
+    exit(1);
+  }
+
+  /* Loop for callback function */
+  pcap_loop(descr, -1, print_packet, NULL);
+
+  return 0;
+}
